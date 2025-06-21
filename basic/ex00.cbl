ca5c3c9a27043abc881fcd515e4527056d3ced87
Nathan <nathan.s.ranna@gmail.com>
2025-06-21 11:50:05 -0300

    change: evaluate to if statement in ex00

--- basic/ex00.cbl
+++ basic/ex00.cbl
@@ -18,8 +18,8 @@
        FILE SECTION.
       ************************************************************************
        WORKING-STORAGE SECTION.
-       01 GDA-NOME      PIC X(20) VALUE SPACES.
-       01 GDA-NUMBER    PIC 9(02) VALUE 0.
+       01 GDA-NOME      PIC X(20)   VALUE SPACES.
+       01 GDA-NUMBER    PIC 9(02)   VALUE 0.
       ************************************************************************
        PROCEDURE DIVISION.
       ************************************************************************
@@ -28,13 +28,12 @@
            ACCEPT GDA-NOME
            DISPLAY "Type a number: "
            ACCEPT GDA-NUMBER
-           EVALUATE GDA-NUMBER
-               WHEN 01
-               WHEN 02
-                   DISPLAY 'Your name is: ' GDA-NOME
-                   DISPLAY 'Number typed: ' GDA-NUMBER
-               WHEN OTHER
-                   DISPLAY "Invalid number."
-           END-EVALUATE
+
+           IF GDA-NUMBER = 1 OR GDA-NUMBER = 2
+               DISPLAY "Your name is: " GDA-NOME
+               DISPLAY "Number typed: " GDA-NUMBER
+           ELSE
+               DISPLAY "Invalid number."
+           END-IF
            STOP RUN.
        END PROGRAM PROG001.
